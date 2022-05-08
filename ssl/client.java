446094192b21afa56c1f18b80b93099917075c20
Nort721 <itag7894@gmail.com>
2022-05-08 21:32:54 +0300

    implemented self-signed certs compatibility

--- ssl/client.java
+++ ssl/client.java
@@ -1,12 +1,18 @@
+package com.nort721.client;
+
 import javax.net.ssl.*;
 import java.io.*;
 import java.security.*;
+import java.security.cert.X509Certificate;
 
 public class client {
 
     private static final String REMOTE_HOST = "localhost";
     private static final int REMOTE_PORT = 1234;
-    private static final String SSL_PASSWORD = "yourPassword";
+
+    private static final String CERT_PASSWORD = "fJpo3hC5N7DntUnv3";
+
+    private static final boolean SERVER_USES_SELF_SIGNED = true;
 
     public static void main(String[] args) {
         new client();
@@ -16,7 +22,7 @@ public class client {
 
         try {
 
-            SSLSocketFactory factory = generateSocketFactory(SSL_PASSWORD.toCharArray());
+            SSLSocketFactory factory = generateSocketFactory(CERT_PASSWORD.toCharArray());
             SSLSocket sslsocket = (SSLSocket) factory.createSocket(REMOTE_HOST,REMOTE_PORT);
 
             // explicitly executing a handshake
@@ -38,25 +44,55 @@ public class client {
             output.close();
             input.close();
             sslsocket.close();
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
     private SSLSocketFactory generateSocketFactory(char[] password) throws Exception {
-        File myCert = new File("myCertificate.jks");
+        File myCert = new File("/Users/nort/All my files/Java projects/RSAClientDemo/clientCertificate.jks");
 
         KeyStore keyStore = KeyStore.getInstance(myCert, password);
 
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        trustManagerFactory.init(keyStore);
+        TrustManager[] trustManagers;
+
+        if (SERVER_USES_SELF_SIGNED) {
+
+            /*
+            Since the server is currently self signing its certificate SSL can't really verify
+            it, so we have to accept all certificates to be able to communicate with it
+
+            we also can't expect any customer to run the command to accept our self-signed
+            server cert, which is why this is pretty much the only option
+             */
+            // ToDo check if its possible to whitelist the server's cert instead of accepting just any cert
+            trustManagers = new TrustManager[] {
+                    new X509TrustManager() {
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+                        public void checkClientTrusted(
+                                java.security.cert.X509Certificate[] certs, String authType) {
+                        }
+                        public void checkServerTrusted(
+                                java.security.cert.X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+        } else {
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(keyStore);
+            trustManagers = trustManagerFactory.getTrustManagers();
+        }
 
         KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("NewSunX509");
         keyManagerFactory.init(keyStore, password);
 
         SSLContext context = SSLContext.getInstance("TLS");// "SSL" or "TLS"
-        context.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+        context.init(keyManagerFactory.getKeyManagers(), trustManagers, null);
+        HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
 
         return context.getSocketFactory();
     }
--- ssl/server.java
+++ ssl/server.java
@@ -1,11 +1,20 @@
+package com.nort721.server;
+
 import javax.net.ssl.*;
 import java.io.*;
 import java.security.KeyStore;
+import java.security.cert.X509Certificate;
 
 public class server {
 
     private static final int PORT = 1234;
-    private static final String SSL_PASSWORD = "yourPassword";
+    private static final String CERT_PASSWORD = "HBTHDgsDSN3uwjFr5";
+
+    // <---> security settings <--->
+    private static final boolean REQUIRE_CLIENT_AUTH = false;
+
+    // <---> properties <--->
+    private static final boolean USING_SELF_SIGNED = true;
 
     public static void main(String[] args) {
         new server();
@@ -15,9 +24,9 @@ public class server {
 
         try {
 
-            SSLServerSocketFactory factory = generateServerFactory(SSL_PASSWORD.toCharArray());
+            SSLServerSocketFactory factory = generateServerFactory(CERT_PASSWORD.toCharArray());
             SSLServerSocket sslserversocket = (SSLServerSocket) factory.createServerSocket(PORT);
-            sslserversocket.setNeedClientAuth(true);
+            sslserversocket.setNeedClientAuth(REQUIRE_CLIENT_AUTH);
 
             System.out.println("listening to secure connections . . .");
 
@@ -34,18 +43,47 @@ public class server {
     }
 
     private SSLServerSocketFactory generateServerFactory(char[] password) throws Exception {
-        File myCert = new File("myCertificate.jks");
+        File myCert = new File("/Users/nort/All my files/Java projects/RSAServerDemo/serverCertificate.jks");
 
         KeyStore keyStore = KeyStore.getInstance(myCert, password);
 
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        trustManagerFactory.init(keyStore);
+        TrustManager[] trustManagers;
+
+        if (REQUIRE_CLIENT_AUTH && USING_SELF_SIGNED) {
+
+            /*
+            Since we are currently self singing our certificates SSL can't really verify
+            them, so we have to accept all certificates, so enabling client auth may be
+            pointless, however it does increase security by a bit since some clients wouldn't
+            agree to auth at all so at least we filter those
+             */
+            // ToDo maybe we can accept the specific cert that all client jars will use on our com.nort721.server.server
+            // ToDo instead of accepting just any cert
+            trustManagers = new TrustManager[] {
+                    new X509TrustManager() {
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+                        public void checkClientTrusted(
+                                java.security.cert.X509Certificate[] certs, String authType) {
+                        }
+                        public void checkServerTrusted(
+                                java.security.cert.X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+        } else {
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(keyStore);
+            trustManagers = trustManagerFactory.getTrustManagers();
+        }
 
         KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("NewSunX509");
         keyManagerFactory.init(keyStore, password);
 
         SSLContext context = SSLContext.getInstance("TLS");// "SSL" or "TLS"
-        context.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+        context.init(keyManagerFactory.getKeyManagers(), trustManagers, null);
 
         return context.getServerSocketFactory();
     }
@@ -85,6 +123,7 @@ public class server {
                 output.close();
                 input.close();
                 socket.close();
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
